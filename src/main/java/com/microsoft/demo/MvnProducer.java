a210f5a682fdcff098bec43eb4684a93a963f50c
onurozardic-ms <124796289+onurozardic-ms@users.noreply.github.com>
2023-10-27 18:58:04 +0300

    first commit

---
+++ src/main/java/com/microsoft/demo/MvnProducer.java
@@ -0,0 +1,13 @@
+package com.microsoft.demo;
+
+/**
+ * Some simple add solution to check maven publishing and consuming packages
+ */
+public class MvnProducer
+{
+    public int add(int a, int b)
+    {
+        return a+b;
+    }
+
+}
---
+++ src/test/java/com/microsoft/demo/MvnProducerTest.java
@@ -0,0 +1,12 @@
+package com.microsoft.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MvnProducerTest {
+
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        // ..
+    }
+}
